fc3c7430421c70c4cf1f92b12fdb486c62dd24a3
jonyglex <18040425@itdurango.edu.mx>
2023-05-03 12:21:43 -0600

    byebye

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import Slider from "infinite-react-carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./Slide.css";
 
-const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+const Slide = ({ children }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    appendDots: (dots) => {
+      return <ul style={{ margin: "0px" }}>{dots}</ul>;
+    },
+  };
   return (
     <div className="slide">
       <div className="containerslide">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-          {children}
-        </Slider>
+        <Slider {...settings}>{children}</Slider>
       </div>
     </div>
   );
