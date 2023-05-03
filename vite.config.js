b71ae66a36e0849e5b29c34af4eade7987850d3d
jonyglex <18040425@itdurango.edu.mx>
2023-05-03 11:51:03 -0600

    base updateed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jonyglex.github.io/fivers/",
+  base: "https://jonyglex.github.io/vite-project/",
 });
